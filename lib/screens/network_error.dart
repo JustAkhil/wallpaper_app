7d741a51c68c600f8c7693a88eed823ddde4b3b1
Akhil <sahuv3694@gmail.com>
2026-05-09 04:31:21 +0530

    New msg

--- lib/screens/network_error.dart
+++ lib/screens/network_error.dart
@@ -46,7 +46,7 @@ class _NetworkErrorPageState extends State<NetworkErrorPage> {
             borderRadius: BorderRadius.circular(18),
           ),
           content: const Text(
-            "Still no internet connection",
+            "Please check your internet connection and try again.",
             style: TextStyle(
               color: Colors.white,
               fontWeight: FontWeight.w600,
